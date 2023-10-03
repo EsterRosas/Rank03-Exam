1e2184ee9ac2d273394b5dfe6425dfe126c5df1a
Ester Rosas Cortada <erosas-c@student.42barcelona.com>
2023-10-03 20:08:17 +0200

    get_next_line OK passed GRADEME

---
+++ get_next_line OK/get_next_line.c
@@ -0,0 +1,33 @@
+#include "get_next_line.h"
+
+char *get_next_line(int fd)
+{
+	char	buff[999999] = {0};
+	int		i = 0;
+	char	*res;
+
+	if (fd < 0 || !BUFFER_SIZE)
+		return (NULL);
+	while (read(fd, &buff[i], 1) == 1)
+	{
+		if (buff[i] == '\n')
+		{
+			i++;
+			break;
+		}
+		i++;
+	}
+	if (!buff[0])
+		return (NULL);
+	res = malloc(i + 1);
+	if (!res)
+		return (NULL);
+	i = 0;
+	while (buff[i])
+	{
+		res[i] = buff[i];
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
+}
